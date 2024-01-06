13ef086248fb4f94f4035363aa0150a1a2415d4a
Steven Lai <2061455536@qq.com>
2024-01-06 14:59:30 +0800

    Update TwoPaneSplitViewHorizontalExposed.cs

--- Editor/Resources/TwoPaneSplitViewHorizontalExposed.cs
+++ Editor/Resources/TwoPaneSplitViewHorizontalExposed.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UIElements;
-
-namespace RPGCore.AI.HFSM
+public class TwoPaneSplitViewHorizontalExposed : TwoPaneSplitView
 {
-	public class TwoPaneSplitViewHorizontalExposed : TwoPaneSplitView
-	{
-		public new class UxmlFactory : UxmlFactory<TwoPaneSplitViewHorizontalExposed, UxmlTraits>
-		{ }
+	public new class UxmlFactory : UxmlFactory<TwoPaneSplitViewHorizontalExposed, UxmlTraits>
+	{ }
 
-		public TwoPaneSplitViewHorizontalExposed()
-		{
-			this.orientation = TwoPaneSplitViewOrientation.Horizontal;
-		}
+	public TwoPaneSplitViewHorizontalExposed()
+	{
+		this.orientation = TwoPaneSplitViewOrientation.Horizontal;
 	}
 }
